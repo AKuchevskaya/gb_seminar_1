bbfe19924b391475cf96eae45bfb10057a09524a
AKuchevskaya <alena.kuchevskaya@yandex.ru>
2023-03-18 19:42:26 +0300

    добавить решение задачи 6

--- Program.cs
+++ Program.cs
@@ -40,18 +40,34 @@
 // -3 -> нет
 // 7 -> нет
 
-Console.Write("Введите первое число: ");
-int number = int.Parse(Console.ReadLine()!);
-if (number % 2 == 0)
-{
-    System.Console.WriteLine($"Число {number} четное");
-}
-else
-{
-    System.Console.WriteLine($"Число {number} нечетное");
-}
+// Console.Write("Введите число: ");
+// int number = int.Parse(Console.ReadLine()!);
+// if (number % 2 == 0)
+// {
+//     System.Console.WriteLine($"Число {number} четное");
+// }
+// else
+// {
+//     System.Console.WriteLine($"Число {number} нечетное");
+// }
 
 // Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+int A = 1;
+while (A <= N)
+{
+    if (A % 2 == 0)
+    {
+        System.Console.Write($"{A} ");
+        A = A + 2;
+    }
+    else
+    {
+        A++;
+    }
+}
